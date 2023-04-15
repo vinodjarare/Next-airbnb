85b46ecc1ca001d168986c219620d44ba0cb42b7
Vinod Jarare <vsjarare@gmail.com>
2023-04-15 09:09:29 +0530

    listing creation (location selection)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,6 +6,8 @@ import { categories } from "../Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import Heading from "../Heading";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -68,6 +70,16 @@ const RentModal = () => {
     },
   });
   const category = watch("category");
+  const location = watch("location");
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
+
   let bodyContent = (
     <div className="flex flex-col gap-8">
       <Heading
@@ -97,11 +109,27 @@ const RentModal = () => {
       </div>
     </div>
   );
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Pick a location"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
